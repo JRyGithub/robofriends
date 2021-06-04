a5931501d3e3e20fd81addf2d77c968190d15b5d
JRyGithub <jryland@protonmail.com>
2021-06-04 17:50:36 +0200

    Adding redux

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
-import 'tachyons';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider, connect } from "react-redux";
+import { createStore } from "redux";
+import "./index.css";
+import App from "./containers/App";
+import * as serviceWorker from "./serviceWorker";
+import "tachyons";
+import { searchRobots } from "./reducers.js";
+
+const store = createStore(searchRobots);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
